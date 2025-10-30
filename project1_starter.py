59f5c9be25f50a430f443810d7aab597f4a68c51
Adams Mutai <adamsmutai@Adamss-MBP.lan>
2025-10-30 11:03:50 -0400

    Tested create character.

--- project1_starter.py
+++ project1_starter.py
@@ -129,7 +129,8 @@ if __name__ == "__main__":
     print("Test your functions here!")
     
     # Example usage:
-    # char = create_character("TestHero", "Warrior")
+    char = create_character("TestHero", "Warrior")
+    print(char)
     # display_character(char)
     # save_character(char, "my_character.txt")
     # loaded = load_character("my_character.txt")
